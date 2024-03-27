0315c70769a5c8b4f53e08f2f0f02b276504ae94
DoSmile0705 <dev.lifecreator@gmail.com>
2024-03-27 12:55:41 -0700

    init

---
+++ src/app/data.js
@@ -0,0 +1,95 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "とあるVの作業部屋",
+    description: "",
+    date: "2023-01-20",
+    demoLink: "https://cluster.mu/w/44e5d42b-e863-498e-8936-689f9ea40c12",
+  },
+  {
+    id: 2,
+    name: "Studio Symbol (L)",
+    description: "",
+    date: "2022-05-30",
+    demoLink: "https://cluster.mu/en/w/d1ab7718-93ae-4133-8942-34f98976bc71",
+  },
+  {
+    id: 3,
+    name: "XYMCity",
+    description: "",
+    date: "2022-02-23",
+    demoLink: "https://cluster.mu/w/a3115279-6d7a-4f6d-99f6-a010eca51e99",
+  },
+  {
+    id: 4,
+    name: "Crypto Bar「Laplace」",
+    description: "",
+    date: "2023-01-29",
+    demoLink: "https://cluster.mu/w/3b00165e-0177-4e56-b6c4-e59b664a0646",
+  },
+  {
+    id: 5,
+    name: "hurel9",
+    description: "",
+    date: "2023-08-29",
+    demoLink: "https://hurel9.itch.io/hurel",
+  },
+  {
+    id: 6,
+    name: "3D-portfolio",
+    description: "",
+    date: "2023-08-13",
+    demoLink: "https://maxime-morel.xyz/",
+  }
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/DoSmile0705",
+    icon: "github",
+    newTab: true,
+  },
+  // {
+  //   label: "LinkedIn",
+  //   link: "https://www.linkedin.com/in/codebucks",
+  //   icon: "linkedin",
+  //   newTab: true,
+  // },
+  // {
+  //   label: "X",
+  //   link: "https://www.x.com/code_bucks",
+  //   icon: "twitter",
+  //   newTab: true,
+  // },
+  {
+    label: "Resume",
+    link: "https://flowcv.com/resume/que99qrkbr",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Web & Unity 3D developer
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            Welcome to my portfolio as a Unity 3D developer.
+            With 7 years of experience in web development and 4 years specializing in Unity 3D development, I bring a wealth of knowledge and expertise to the table. My skills extend across various fields, including metaverse and online games, allowing me to tackle diverse and exciting projects. I thrive in collaborative environments and actively engage with the developer community.
+
+            If you're seeking creative and innovative solutions, partnering with me will be a rewarding experience. I am committed to delivering optimized and advanced products that exceed expectations. Your vision will be transformed into a captivating reality through my dedication and skill.
+
+            Thank you for considering me as your developer. I am eager to embark on new ventures and contribute to your success. Let's create something extraordinary together.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-2 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            15+ <sub className="font-semibold text-base">clients</sub>
+          </p>
+        </ItemLayout>
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-2 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            40+{" "}
+            <sub className="font-semibold text-base">projects</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            7+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        {/* <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=fullsuccess.world&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="Minato"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=fullsuccess.world&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="Minato"
+            loading="lazy"
+          />
+        </ItemLayout> */}
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
+            alt="Minato"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        {/* <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=fullsuccess.world&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="Minato"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/fullsuccess.world/Nextjs-contentlayer-blog"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=fullsuccess.world&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="Minato"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout> */}
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
