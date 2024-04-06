083ef0e1d382b05416c417258adfada70f794930
DoSmile0705 <dev.lifecreator@gmail.com>
2024-04-05 18:44:30 -0700

    addd: flutter game

--- src/app/data.js
+++ src/app/data.js
@@ -19,46 +19,62 @@ Websites:
 export const projectsData = [
   {
     id: 1,
+    name: "Flame Engine を使用したシンプルなアーケード ゲーム",
+    description: "",
+    date: "2023-11-14",
+    demoLink:
+      "https://flutterawesome.com/a-simple-arcade-game-using-flame-engine/",
+  },
+  {
+    id: 2,
+    name: "YuNO - ミニマリストのオープンソース レトロ ゲーム フロントエンド",
+    description: "",
+    date: "2024-01-01",
+    demoLink:
+      "https://flutterawesome.com/yuno-a-minimalist-open-source-retro-gaming-frontend/",
+  },
+  {
+    id: 3,
     name: "とあるVの作業部屋",
     description: "",
     date: "2023-01-20",
     demoLink: "https://cluster.mu/w/44e5d42b-e863-498e-8936-689f9ea40c12",
   },
   {
-    id: 2,
+    id: 4,
     name: "Studio Symbol (L)",
     description: "",
     date: "2022-05-30",
     demoLink: "https://cluster.mu/en/w/d1ab7718-93ae-4133-8942-34f98976bc71",
   },
   {
-    id: 3,
+    id: 5,
     name: "XYMCity",
     description: "",
     date: "2022-02-23",
     demoLink: "https://cluster.mu/w/a3115279-6d7a-4f6d-99f6-a010eca51e99",
   },
   {
-    id: 4,
+    id: 6,
     name: "Crypto Bar「Laplace」",
     description: "",
     date: "2023-01-29",
     demoLink: "https://cluster.mu/w/3b00165e-0177-4e56-b6c4-e59b664a0646",
   },
   {
-    id: 5,
+    id: 7,
     name: "hurel9",
     description: "",
     date: "2023-08-29",
     demoLink: "https://hurel9.itch.io/hurel",
   },
   {
-    id: 6,
+    id: 8,
     name: "3D-portfolio",
     description: "",
     date: "2023-08-13",
     demoLink: "https://maxime-morel.xyz/",
-  }
+  },
 ];
 
 export const BtnList = [
